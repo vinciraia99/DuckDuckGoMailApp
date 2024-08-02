3d1477c5792cca448a8d61e4d0fb284335bfa404
vinciraia99 <99raia99@gmail.com>
2024-08-03 01:59:28 +0200

    refactor ui

--- lib/email_protection.dart
+++ lib/email_protection.dart
@@ -1,10 +1,9 @@
+import 'package:duckduckgoemail/email_generate.dart';
 import 'package:flutter/material.dart';
-import 'package:duckduckgoemail/email_generate.dart'; // Assicurati di avere i percorsi corretti
 import 'api_service.dart';
 import 'login_screen.dart';
 
-class EmailInboxScreen extends StatelessWidget {
-  final TextEditingController _otpController = TextEditingController();
+class EmailInboxScreen extends StatefulWidget {
   final String email;
   final VoidCallback toggleTheme;
   final ThemeMode themeMode;
@@ -16,115 +15,166 @@ class EmailInboxScreen extends StatelessWidget {
   });
 
   @override
+  _EmailInboxScreenState createState() => _EmailInboxScreenState();
+}
+
+class _EmailInboxScreenState extends State<EmailInboxScreen> {
+  final TextEditingController _otpController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 20),
-              const Text(
-                'Check your inbox!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Open the link we sent to $email in this browser or enter the one-time passphrase below.',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _otpController,
-                decoration: InputDecoration(
-                  filled: true,
-                  hintText: 'Enter your one-time passphrase',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_otpController.text.isNotEmpty) {
-                    login(email, _otpController.text).then((success) {
-                      if (success.isNotEmpty) {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => EmailProtectionScreen(
-                              username: email,
-                              tokenMail: success,
-                              toggleTheme: toggleTheme,
-                              themeMode: themeMode,
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/duck.png', // Percorso dell'immagine dell'icona
+              height: 24,
+              width: 24,
+            ),
+            const SizedBox(width: 8),
+            const Text('Account verification'),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              widget.themeMode == ThemeMode.light ? Icons.dark_mode : Icons.light_mode,
+            ),
+            onPressed: widget.toggleTheme,
+          ),
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  width: constraints.maxWidth < 600 ? double.infinity : 400,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/email.png', // Percorso dell'immagine
+                        height: 150, // Altezza dell'immagine
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Check your inbox!',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        'Open the link we sent to ${widget.email} in this app or enter the one-time passphrase below.',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: _otpController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          hintText: 'Enter your one-time passphrase',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50, // Aumenta l'altezza del pulsante di login
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue, // Colore di sfondo
+                            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
                             ),
                           ),
-                        );
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              content: Text('Login failed. Please try again.'),
-                            );
+                          child: const Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontSize: 18, // Aumenta la dimensione del testo
+                              color: Colors.white, // Colore del testo
+                            ),
+                          ),
+                          onPressed: () {
+                            if (_otpController.text.isNotEmpty) {
+                              login(widget.email, _otpController.text).then((success) {
+                                if (success.isNotEmpty) {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => EmailProtectionScreen(
+                                        username: widget.email,
+                                        tokenMail: success,
+                                        toggleTheme: widget.toggleTheme,
+                                        themeMode: widget.themeMode,
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        content: Text('Login failed. Please try again.'),
+                                      );
+                                    },
+                                  );
+                                }
+                              });
+                            }
                           },
-                        );
-                      }
-                    });
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue, // Background color
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: Text('Sign In'),
-              ),
-              SizedBox(height: 20),
-              TextButton(
-                onPressed: () {
-                  // Handle resend action
-                },
-                child: Text(
-                  "Didn't get the email? Resend",
-                  style: TextStyle(
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LoginScreen(
-                        toggleTheme: toggleTheme,
-                        themeMode: themeMode,
+                        ),
                       ),
-                    ),
-                  );
-                },
-                child: const Text(
-                  "Mail not correct? Go back",
-                  style: TextStyle(
-                    color: Colors.blue,
+                      const SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () {
+                          // Gestisci l'azione di reinvio
+                        },
+                        child: const Text(
+                          "Didn't get the email? Resend",
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(
+                                toggleTheme: widget.toggleTheme,
+                                themeMode: widget.themeMode,
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Mail not correct? Go back",
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'api_service.dart';
-import 'home_screen.dart';
 import 'email_protection.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -18,7 +17,17 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/duck.png',
+              height: 24,
+              width: 24,
+            ),
+            const SizedBox(width: 8),
+            const Text('Login'),
+          ],
+        ),
         actions: [
           IconButton(
             icon: Icon(
@@ -28,85 +37,121 @@ class LoginScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/duck.png',
-              height: 150,
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              child: const Text('Login'),
-              onPressed: () {
-                // Simula il login per scopi dimostrativi
-                if (_emailController.text.isNotEmpty) {
-                  if (kDebugMode) {
-                    print(_emailController.text);
-                  }
-                  try {
-                    loginRequest(_emailController.text).then((success) {
-                      if (success) {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => EmailInboxScreen(
-                              email: _emailController.text,
-                              toggleTheme: toggleTheme,
-                              themeMode: themeMode,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  width: constraints.maxWidth < 600 ? double.infinity : 400,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/email_lock.png',
+                        height: 150,
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50, // Aumenta l'altezza del pulsante di login
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue, // Colore di sfondo
+                            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
                             ),
                           ),
-                        );
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return const AlertDialog(
-                              content: Text('Email o Password errata'),
-                            );
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 18, // Aumenta la dimensione del testo
+                              color: Colors.white, // Colore del testo
+                            ),
+                          ),
+                          onPressed: () {
+                            // Simula il login per scopi dimostrativi
+                            if (_emailController.text.isNotEmpty) {
+                              if (kDebugMode) {
+                                print(_emailController.text);
+                              }
+                              try {
+                                loginRequest(_emailController.text).then((success) {
+                                  if (success) {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => EmailInboxScreen(
+                                          email: _emailController.text,
+                                          toggleTheme: toggleTheme,
+                                          themeMode: themeMode,
+                                        ),
+                                      ),
+                                    );
+                                  } else {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return const AlertDialog(
+                                          content: Text('Email o Password errata'),
+                                        );
+                                      },
+                                    );
+                                  }
+                                }).catchError((error) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        content: Text('Si è verificato un errore: $error'),
+                                      );
+                                    },
+                                  );
+                                });
+                              } catch (error) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      content: Text('Si è verificato un errore: $error'),
+                                    );
+                                  },
+                                );
+                              }
+                            } else {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return const AlertDialog(
+                                    content: Text('Email o Password errata'),
+                                  );
+                                },
+                              );
+                            }
                           },
-                        );
-                      }
-                    }).catchError((error) {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            content: Text('Si è verificato un errore: $error'),
-                          );
-                        },
-                      );
-                    });
-                  } catch (error) {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: Text('Si è verificato un errore: $error'),
-                        );
-                      },
-                    );
-                  }
-                } else {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return const AlertDialog(
-                        content: Text('Email o Password errata'),
-                      );
-                    },
-                  );
-                }
-              },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
